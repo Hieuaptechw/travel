1ac531c2ba26963df3639f7d3217e27cfb6e11bb
akared45 <vinhle5620@gmail.com>
2024-09-18 16:15:58 +0700

    home
    home

--- travel-smart/src/pages/user/layout/header/Header.jsx
+++ travel-smart/src/pages/user/layout/header/Header.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Navbar, Nav, Container, NavDropdown, Button, Offcanvas, Form, FormControl } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown, Button, Offcanvas } from 'react-bootstrap';
 import image from '../../../../assets/images/logo.jpg';
 import './Header.scss';
 
 const Header = () => {
-    const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
-
     const [showOffcanvas, setShowOffcanvas] = useState(false);
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
@@ -39,27 +37,27 @@ const Header = () => {
                     {/* Navbar Menu for large screens */}
                     <Navbar.Collapse id="navbar-nav" className="d-none d-lg-flex justify-content-between">
                         <Nav className="mx-auto">
-                            <Nav.Link eventKey={1} href="#">Home</Nav.Link>
+                            <Nav.Link href="#">Home</Nav.Link>
                             <NavDropdown title="Categories" id="categories-dropdown">
-                                <NavDropdown.Item eventKey={2.1} href="#">Hotel</NavDropdown.Item>
-                                <NavDropdown.Item eventKey={2.2} href="#">Tour</NavDropdown.Item>
-                                <NavDropdown.Item eventKey={2.3} href="#">Activity</NavDropdown.Item>
-                                <NavDropdown.Item eventKey={2.4} href="#">Holiday Rentals</NavDropdown.Item>
-                                <NavDropdown.Item eventKey={2.5} href="#">Car</NavDropdown.Item>
-                                <NavDropdown.Item eventKey={2.6} href="#">Cruise</NavDropdown.Item>
-                                <NavDropdown.Item eventKey={2.7} href="#">Flight</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Hotel</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Tour</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Activity</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Holiday Rentals</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Car</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Cruise</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Flight</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link eventKey={3} href="#">Destinations</Nav.Link>
-                            <Nav.Link eventKey={4} href="#">Blogs</Nav.Link>
+                            <Nav.Link href="#">Destinations</Nav.Link>
+                            <Nav.Link href="#">Blogs</Nav.Link>
                             <NavDropdown title="Pages" id="pages-dropdown">
-                                <NavDropdown.Item eventKey={5.1} href="#">About</NavDropdown.Item>
-                                <NavDropdown.Item eventKey={5.2} href="#">Terms</NavDropdown.Item>
+                                <NavDropdown.Item href="#">About</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Terms</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link eventKey={6} href="#">Contact</Nav.Link>
+                            <Nav.Link href="#">Contact</Nav.Link>
                         </Nav>
                         <div className="d-flex align-items-center">
-                            <Button variant="danger" className="ms-2">SIGN IN</Button>
-                            <Button variant="primary" className="ms-2">LOG IN</Button>
+                            <Button variant="outline-light" className="header-button ms-2">SIGN IN</Button>
+                            <Button variant="primary" className="header-button ms-2">LOG IN</Button>
                         </div>
                     </Navbar.Collapse>
 
@@ -70,25 +68,27 @@ const Header = () => {
                         </Offcanvas.Header>
                         <Offcanvas.Body>
                             <Nav className="flex-column">
-                                <Nav.Link eventKey={1} href="#">Home</Nav.Link>
+                                <Nav.Link href="#">Home</Nav.Link>
                                 <NavDropdown title="Categories" id="categories-dropdown">
-                                    <NavDropdown.Item eventKey={2.1} href="#">Hotel</NavDropdown.Item>
-                                    <NavDropdown.Item eventKey={2.2} href="#">Tour</NavDropdown.Item>
-                                    <NavDropdown.Item eventKey={2.3} href="#">Activity</NavDropdown.Item>
-                                    <NavDropdown.Item eventKey={2.4} href="#">Holiday Rentals</NavDropdown.Item>
-                                    <NavDropdown.Item eventKey={2.5} href="#">Car</NavDropdown.Item>
-                                    <NavDropdown.Item eventKey={2.6} href="#">Cruise</NavDropdown.Item>
-                                    <NavDropdown.Item eventKey={2.7} href="#">Flight</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">Hotel</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">Tour</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">Activity</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">Holiday Rentals</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">Car</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">Cruise</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">Flight</NavDropdown.Item>
                                 </NavDropdown>
-                                <Nav.Link eventKey={3} href="#">Destinations</Nav.Link>
-                                <Nav.Link eventKey={4} href="#">Blogs</Nav.Link>
+                                <Nav.Link href="#">Destinations</Nav.Link>
+                                <Nav.Link href="#">Blogs</Nav.Link>
                                 <NavDropdown title="Pages" id="pages-dropdown">
-                                    <NavDropdown.Item eventKey={5.1} href="#">About</NavDropdown.Item>
-                                    <NavDropdown.Item eventKey={5.2} href="#">Terms</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">About</NavDropdown.Item>
+                                    <NavDropdown.Item href="#">Terms</NavDropdown.Item>
                                 </NavDropdown>
-                                <Nav.Link eventKey={6} href="#">Contact</Nav.Link>
-                                <Button variant="danger" className="mt-2">SIGN IN</Button>
-                                <Button variant="primary" className="mt-2">LOG IN</Button>
+                                <Nav.Link href="#">Contact</Nav.Link>
+                                <div className="d-flex flex-column">
+                                    <Button variant="outline-light" className="mt-2">SIGN IN</Button>
+                                    <Button variant="primary" className="mt-2">LOG IN</Button>
+                                </div>
                             </Nav>
                         </Offcanvas.Body>
                     </Offcanvas>
