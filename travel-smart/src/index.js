ecfc012a391c1c6c692693264a9b432e0c9eaace
Hieuaptechw <phihieu6789a@gmail.com>
2024-09-18 08:55:38 +0700

    admin
    admin

---
+++ travel-smart/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import RouterCustom from './router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+// import './style/style.css';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   
+ <BrowserRouter>
+ <RouterCustom/>
+ </BrowserRouter>
+);
+reportWebVitals();
