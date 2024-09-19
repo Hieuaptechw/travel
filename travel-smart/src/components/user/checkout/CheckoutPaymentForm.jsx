b73d22d9b38454af8d5dbbdf412a502a38cbea16
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2024-09-20 00:19:18 +0700

    Payment method

--- travel-smart/src/components/user/checkout/CheckoutPaymentForm.jsx
+++ travel-smart/src/components/user/checkout/CheckoutPaymentForm.jsx
@@ -11,6 +11,48 @@ function CheckoutPaymentForm() {
                     your payment processor's fees after this step.
                 </div>
             </div>
+            <div class="col-12">
+                <div class="d-flex align-items-center fs-3">
+                    <input class="form-check-input mt-0 me-3" type="radio" 
+                        name="payment-type" value="paypal" id="payment-type-paypal" />
+                    <label for="payment-type-paypal">Pay with PayPal</label>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="d-flex align-items-center fs-3">
+                    <input class="form-check-input mt-0 me-3" type="radio" 
+                        name="payment-type" value="cash-app" id="payment-type-cash-app" />
+                    <label for="payment-type-cash-app">Pay with Cash App</label>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="d-flex align-items-center fs-3">
+                    <input class="form-check-input mt-0 me-3" type="radio" 
+                        name="payment-type" value="zalo-pay" id="payment-type-zalo-pay" />
+                    <label for="payment-type-zalo-pay">Pay with Zalo Pay</label>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="d-flex align-items-center fs-3">
+                    <input class="form-check-input mt-0 me-3" type="radio" 
+                        name="payment-type" value="crypto" id="payment-type-crypto" />
+                    <label for="payment-type-crypto">Pay with some obscure cryptocurrency</label>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="d-flex align-items-center fs-3">
+                    <input class="form-check-input mt-0 me-3" type="radio" 
+                        name="payment-type" value="card" id="payment-type-card" />
+                    <label for="payment-type-card">Pay with credit/debit card</label>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="d-flex align-items-center fs-3">
+                    <input class="form-check-input mt-0 me-3" type="radio" 
+                        name="payment-type" value="cash" id="payment-type-cash" />
+                    <label for="payment-type-cash">Pay with cash</label>
+                </div>
+            </div>
             <div class="mb-0"></div>
         </form>
     )
