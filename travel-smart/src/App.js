c635731012fba6b491a1e10c695008edb93dd459
akared45 <vinhle5620@gmail.com>
2024-09-21 10:06:27 +0700

    Header

--- travel-smart/src/App.js
+++ travel-smart/src/App.js
@@ -2,12 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import RouterCustom from './router';
+import { createContext, useMemo, useState } from 'react';
+
+export let GlobalContext = createContext({
+  user: null,
+  setUser: null,
+});
 
 function App() {
+  const [user, setUser] = useState(window.localStorage.getItem("user"));
+
+  const globalValue = useMemo(() => ({
+    user, setUser,
+  }), [
+    user, setUser,
+  ]);
+
   return (
-    <BrowserRouter>
-      <RouterCustom/>
-    </BrowserRouter>   
+    <GlobalContext.Provider value={globalValue}>
+      <BrowserRouter>
+        <RouterCustom/>
+      </BrowserRouter>  
+    </GlobalContext.Provider> 
   );
 }
 
--- travel-smart/src/pages/user/layout/header/Header.jsx
+++ travel-smart/src/pages/user/layout/header/Header.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { Container, Row, Navbar, Nav,Col } from "react-bootstrap";
 import image from '../../../../assets/images/logo.png';
 import './Header.scss';
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { Icon } from "@iconify-icon/react/dist/iconify.mjs";
+import { GlobalContext } from "../../../../App";
 
 const Header = () => {
+
+    const global = useContext(GlobalContext);
 
     const [mobileActive, setMobileActive] = useState(false);
     const [activeNav, setActiveNav] = useState("");
@@ -19,8 +22,11 @@ const Header = () => {
     }
 
     useEffect(() => {
+        if (!activeNav) setMobileActive(false);
+    }, [activeNav]);
+
+    useEffect(() => {
         setActiveNav("");
-        setMobileActive(false);
     }, [location])
 
     const handleNavbarItemRollover = (item) => {
@@ -34,6 +40,9 @@ const Header = () => {
     }
     const handleHeaderMouseExit = () => {
         if (canRollover().matches) setActiveNav("");
+    }
+    const handleHeaderClickOut = () => {
+        setActiveNav("");
     }
     
     const handleMobileToggleActive = () => {
@@ -94,16 +103,29 @@ const Header = () => {
                             />
                     </button>
                     <div className="navbar-space flex-fill" onPointerEnter={handleHeaderMouseExit}>&nbsp;</div>
-                    <Nav id="navbar-nav-user">
-                        <Link to="/login" className="nav-link">Log in</Link>
-                        <Link to="/signup" className="nav-link sign-up-button">Sign up</Link>
-                    </Nav>
+                    {
+                        global.user ? <>
+                            <Nav id="navbar-nav-user" class="navbar-nav signed-in">
+                                <Link to="/checkout" className="nav-link">
+                                    USD <b>1,008.40</b>
+                                </Link>
+                            </Nav>
+                            <Link to="/profile" id="profile-btn" className="btn">
+                                <Icon icon="bi:person"  />
+                            </Link>
+                        </> : <>
+                            <Nav id="navbar-nav-user">
+                                <Link to="/login" className="nav-link">Log in</Link>
+                                <Link to="/signup" className="nav-link sign-up-button">Sign up</Link>
+                            </Nav>
+                        </>
+                    }
                 </Container>
             </Navbar>
             <div id="nav-bg" className={
                 ""
                 + (activeNav ? " active" : "")
-            } />
+            } onPointerLeave={handleHeaderMouseExit} onPointerDown={handleHeaderClickOut} />
             <section id="nav-explore" className={
                 "header-section"
                 + (activeNav === "explore" ? " active" : "")
@@ -117,7 +139,7 @@ const Header = () => {
                         <div className="col-12 col-lg-9">
                             <div className="row g-2">
                                 <div className="col-12 col-sm-6 col-md-4">
-                                    <Link className="card h-100 text-decoration-none" to="/tours">
+                                    <Link className="header-category card h-100 text-decoration-none" to="/tours">
                                         <img className="card-img-top" src="https://img.hieuaptech.com/Banner/tour.jpg" />
                                         <div class="card-body">
                                             <h5 class="card-title">Tours</h5>
@@ -126,7 +148,7 @@ const Header = () => {
                                     </Link>
                                 </div>
                                 <div className="col-12 col-sm-6 col-md-4">
-                                    <Link className="card h-100 text-decoration-none" to="/locations">
+                                    <Link className="header-category card h-100 text-decoration-none" to="/locations">
                                         <img className="card-img-top" src="https://img.hieuaptech.com/Banner/landmarks.jpg" />
                                         <div class="card-body">
                                             <h5 class="card-title">Landmarks</h5>
@@ -152,7 +174,7 @@ const Header = () => {
                         <div className="col-12 col-lg-9">
                             <div className="row g-2">
                                 <div className="col-12 col-sm-6 col-md-4">
-                                    <Link className="card h-100 text-decoration-none" to="/hotels">
+                                    <Link className="header-category card h-100 text-decoration-none" to="/hotels">
                                         <img className="card-img-top" src="https://img.hieuaptech.com/Hotel/Paris.jpg" />
                                         <div class="card-body">
                                             <h5 class="card-title">Hotels</h5>
@@ -161,7 +183,7 @@ const Header = () => {
                                     </Link>
                                 </div>
                                 <div className="col-12 col-sm-6 col-md-4">
-                                    <Link className="card h-100 text-decoration-none" to="/rentals">
+                                    <Link className="header-category card h-100 text-decoration-none" to="/rentals">
                                         <img className="card-img-top" src="https://img.hieuaptech.com/Banner/rentals.jpg " />
                                         <div class="card-body">
                                             <h5 class="card-title">Rentals</h5>
@@ -170,7 +192,7 @@ const Header = () => {
                                     </Link>
                                 </div>
                                 <div className="col-12 col-sm-6 col-md-4">
-                                    <Link className="card h-100 text-decoration-none" to="/cruises">
+                                    <Link className="header-category card h-100 text-decoration-none" to="/cruises">
                                         <img className="card-img-top" src="https://ik.imagekit.io/tvlk/xpe-asset/AyJ40ZAo1DOyPyKLZ9c3RGQHTP2oT4ZXW+QmPVVkFQiXFSv42UaHGzSmaSzQ8DO5QIbWPZuF+VkYVRk6gh-Vg4ECbfuQRQ4pHjWJ5Rmbtkk=/6897813827256/-Route-2-2D1N-Halong-Bay-by-Alisa-Premier-Luxury-5-Stars-Cruise-680d19e6-2ff0-4836-91f6-9da51ac47684.jpeg?tr=q-60,c-at_max,w-1280,h-720&_src=imagekit" />
                                         <div class="card-body">
                                             <h5 class="card-title">Cruises</h5>
@@ -196,7 +218,7 @@ const Header = () => {
                         <div className="col-12 col-lg-9">
                             <div className="row g-2">
                                 <div className="col-12 col-sm-6 col-md-4">
-                                    <Link className="card h-100 text-decoration-none" to="/flights">
+                                    <Link className="header-category card h-100 text-decoration-none" to="/flights">
                                         <img className="card-img-top" src="https://afar.brightspotcdn.com/dims4/default/3f96d36/2147483647/strip/true/crop/2441x1627+62+0/resize/900x600!/quality/90/?url=https%3A%2F%2Fk3-prod-afar-media.s3.us-west-2.amazonaws.com%2Fbrightspot%2F10%2F8c%2F4c8621923b7e191eb2c232bca3fd%2Foriginal-shutterstock-345664409.jpg" />
                                         <div class="card-body">
                                             <h5 class="card-title">Flights</h5>
@@ -205,7 +227,7 @@ const Header = () => {
                                     </Link>
                                 </div>
                                 <div className="col-12 col-sm-6 col-md-4">
-                                    <Link className="card h-100 text-decoration-none" to="/trains">
+                                    <Link className="header-category card h-100 text-decoration-none" to="/trains">
                                         <img className="card-img-top" src="https://www.hsrail.org/wp-content/uploads/2024/05/Frecciarossa_South_of_Milan_Harnish_900x600.jpg" />
                                         <div class="card-body">
                                             <h5 class="card-title">Trains</h5>
@@ -214,7 +236,7 @@ const Header = () => {
                                     </Link>
                                 </div>
                                 <div className="col-12 col-sm-6 col-md-4">
-                                    <Link className="card h-100 text-decoration-none" to="/cars">
+                                    <Link className="header-category card h-100 text-decoration-none" to="/cars">
                                         <img className="card-img-top" src="https://mezha.media/wp-content/uploads/2024/09/DS-SM-Tribute-900x600.jpg" />
                                         <div class="card-body">
                                             <h5 class="card-title">Car</h5>
@@ -244,8 +266,8 @@ const Header = () => {
                         <div className="col-12 col-lg-9">
                             <div className="row g-2">
                                 <h3>Latest stories</h3>
-                                <div className="col-12 col-md-4">
-                                    <Link className="card h-100 text-decoration-none" to="/cars">
+                                <div className="col-12 col-lg-6">
+                                    <Link className="header-blog card h-100 text-decoration-none" to="/cars">
                                         <img className="card-img-top" src="https://img.hieuaptech.com/Banner/7.png" />
                                         <div class="card-body px-3 py-2">
                                             <h5 class="card-title">Top 10 locations in France to go to this holiday season</h5>
@@ -253,20 +275,29 @@ const Header = () => {
                                         </div>
                                     </Link>
                                 </div>
-                                <div className="col-12 col-md-4">
-                                    <Link className="card h-100 text-decoration-none" to="/cars">
+                                <div className="col-12 col-lg-6">
+                                    <Link className="header-blog card h-100 text-decoration-none" to="/cars">
                                         <img className="card-img-top" src="https://img.hieuaptech.com/Banner/6.png" />
                                         <div class="card-body px-3 py-2">
-                                            <h5 class="card-title">TWhere can I go? 5 amazing countries that are open right now</h5>
+                                            <h5 class="card-title">Where can I go? 5 amazing countries that are open right now</h5>
                                             <p class="card-subtitle mb-0 opacity-75">You should not miss this, deals are included</p>
                                         </div>
                                     </Link>
                                 </div>
-                                <div className="col-12 col-md-4">
-                                    <Link className="card h-100 text-decoration-none" to="/cars">
+                                <div className="col-12 col-lg-6">
+                                    <Link className="header-blog card h-100 text-decoration-none" to="/cars">
                                         <img className="card-img-top" src="https://img.hieuaptech.com/Banner/5.png" />
                                         <div class="card-body px-3 py-2">
                                             <h5 class="card-title">Booking travel during Corona: good advice in an uncertain time</h5>
+                                            <p class="card-subtitle mb-0 opacity-75">You should not miss this, deals are included</p>
+                                        </div>
+                                    </Link>
+                                </div>
+                                <div className="col-12 col-lg-6">
+                                    <Link className="header-blog card h-100 text-decoration-none" to="/cars">
+                                        <img className="card-img-top" src="https://img.hieuaptech.com/Banner/8.png" />
+                                        <div class="card-body px-3 py-2">
+                                            <h5 class="card-title">Safety tips to take note of when going to the beaches</h5>
                                             <p class="card-subtitle mb-0 opacity-75">You should not miss this, deals are included</p>
                                         </div>
                                     </Link>
